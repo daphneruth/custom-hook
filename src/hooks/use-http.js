6eb3d99e2ab57f779a5f38fad4fc36829314de8a
Daphne <daphneruth304@gmail.com>
2022-07-16 16:22:18 +0300

    updated return objects

--- src/hooks/use-http.js
+++ src/hooks/use-http.js
@@ -32,9 +32,9 @@ const useHttp = (requestConfig, applyData)=> {
     };
   
    return {
-    isLoading: isLoading,
-    error:error,
-    sendRequest:sendRequest
+    isLoading,
+    error,
+    sendRequest
 
    
     };
